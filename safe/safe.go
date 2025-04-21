8493e35e2a12bd399d54d2f70474edff88f1fa2a
aleksandr <ugolkov.prog@gmail.com>
2025-04-21 19:18:35 +0300

    safe add context and chan

--- safe/safe.go
+++ safe/safe.go
@@ -1,6 +1,8 @@
 package safe
 
 import (
+	"context"
+
 	"github.com/av-ugolkov/gopkg/logger"
 )
 
@@ -14,4 +16,35 @@ func Go(fn func()) {
 
 		fn()
 	}()
+}
+
+func GoCtx(ctx context.Context, fn func(ctx context.Context)) {
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				logger.Errorf("safe.SafeGo: %v", err)
+			}
+		}()
+
+		fn(ctx)
+	}()
+}
+
+func GoCh[T any](ctx context.Context, fn func(ctx context.Context) (T, error)) (chan T, chan error) {
+	dataCh := make(chan T)
+	errCh := make(chan error)
+
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				logger.Errorf("safe.SafeGo: %v", err)
+			}
+		}()
+
+		data, err := fn(ctx)
+		dataCh <- data
+		errCh <- err
+	}()
+
+	return dataCh, errCh
 }
